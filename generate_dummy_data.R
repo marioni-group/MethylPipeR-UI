8adb19721e5c42b539f9ce06a1a00686c2dc9ebe
CHENG Yipeng <s2092119@ed.ac.uk>
2021-08-24 15:49:07 +0100

    Added generation of time-to-event data.

--- generate_dummy_data.R
+++ generate_dummy_data.R
@@ -14,6 +14,12 @@ dummyTestYBinary <- as.numeric(plogis(dummyTestY, location = mean(dummyTestY)) >
 
 dummyCovariates <- matrix(rnorm(300), nrow = 100)
 
+dummyTrainTTE <- runif(100, min = 0, max = 15)
+dummyTestTTE <- runif(100, min = 0, max = 15)
+
+dummyTrainSurv <- data.frame('Event' = dummyTrainYBinary, 'time_to_event' = dummyTrainTTE)
+dummyTestSurv <- data.frame('Event' = dummyTestYBinary, 'time_to_event' = dummyTestTTE)
+
 saveRDS(dummyTrainXs, 'dummy_data/dummyTrainXs.rds')
 saveRDS(dummyTrainY, 'dummy_data/dummyTrainY.rds')
 saveRDS(dummyTestXs, 'dummy_data/dummyTestXs.rds')
@@ -21,3 +27,5 @@ saveRDS(dummyTestY, 'dummy_data/dummyTestY.rds')
 saveRDS(dummyCovariates, 'dummy_data/dummyCovariates.rds')
 saveRDS(dummyTestYBinary, 'dummy_data/dummyTrainYBinary.rds')
 saveRDS(dummyTestYBinary, 'dummy_data/dummyTestYBinary.rds')
+saveRDS(dummyTrainSurv, 'dummy_data/dummyTrainSurv.rds')
+saveRDS(dummyTestSurv, 'dummy_data/dummyTestSurv.rds')
